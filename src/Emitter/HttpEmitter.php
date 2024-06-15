4d75f5bc58a68d1f24b2e7d3ae7dc4104b46f326
Sebastian Pająk <spconv@gmail.com>
2024-06-15 11:37:35 +0200

    Update dependencies. Move send function to emitter

--- src/Emitter/HttpEmitter.php
+++ src/Emitter/HttpEmitter.php
@@ -3,7 +3,6 @@
 namespace Flow\Emitter;
 
 use Psr\Http\Message\ResponseInterface;
-use function Http\Response\send;
 
 /**
  * Emit response to HTTP
@@ -14,6 +13,29 @@ class HttpEmitter implements EmitterInterface
 {
     public function emit(ResponseInterface $response): void
     {
-        send($response);
+        $httpLine = sprintf(
+            'HTTP/%s %s %s',
+            $response->getProtocolVersion(),
+            $response->getStatusCode(),
+            $response->getReasonPhrase()
+        );
+
+        header($httpLine, true, $response->getStatusCode());
+
+        foreach ($response->getHeaders() as $name => $values) {
+            foreach ($values as $value) {
+                header("$name: $value", false);
+            }
+        }
+
+        $stream = $response->getBody();
+
+        if ($stream->isSeekable()) {
+            $stream->rewind();
+        }
+
+        while (!$stream->eof()) {
+            echo $stream->read(1024 * 8);
+        }
     }
 }
